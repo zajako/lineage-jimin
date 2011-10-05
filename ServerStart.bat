37697b3781759b7ee4a61f686d9f4412688b9ab0
wlals1978 <wlals1978@nate.com>
2011-10-05 10:27:49 +0000

    아래와 같은.

--- ServerStart.bat
+++ ServerStart.bat
@@ -1,2 +1,2 @@
-@java -Xmx512m -Xincgc -cp  l1jserver.jar;lib\xmlapi;lib\c3p0-0.9.1.2.jar;lib\mysql-connector-java-5.1.7-bin.jar;lib\javolution.jar server.manager.eva
+@java -Xmx512m -Xincgc -cp  bone.jar;lib\xmlapi;lib\c3p0-0.9.1.2.jar;lib\mysql-connector-java-5.1.7-bin.jar;lib\javolution.jar server.manager.eva
 @pause
